a5fd99f7699c2a6bce4756de6787f6cad784e354
zsoltzsigmondi <zszs@techtalk.at>
2013-08-12 17:19:19 +0200

    Initial steps in implementing the Client side

--- SpecLog.HtmlExportPlugin.Client/HtmlExportPlugin.cs
+++ SpecLog.HtmlExportPlugin.Client/HtmlExportPlugin.cs
@@ -2,10 +2,66 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TechTalk.SpecLog.Application.Common.Dialogs;
+using TechTalk.SpecLog.Application.Common.PluginsInfrastructure;
+using TechTalk.SpecLog.Common;
+using TechTalk.SpecLog.Entities;
 
 namespace SpecLog.HtmlExportPlugin.Client
 {
-    public class HtmlExportPlugin
+    [Plugin(HtmlExportPlugin.PluginName)]
+    public class HtmlExportPlugin : IClientPlugin
     {
+        public const string PluginName = "SpecLog.HtmlExportPlugin";
+
+        public string Name
+        {
+            get { return PluginName; }
+        }
+
+        public string Description
+        {
+            get { return "Periodically generates an HTML export of the whole repository."; }
+        }
+
+        public bool IsConfigurable(RepositoryMode repositoryMode)
+        {
+            return repositoryMode == RepositoryMode.ClientServer;
+        }
+
+        public IDialogViewModel GetConfigDialog(RepositoryMode repositoryMode, bool isEnabled, string configuration)
+        {
+            return new HtmlExportPluginConfigDialogViewModel(configuration, isEnabled);
+        }
+
+        public bool IsGherkinLinkProvider(RepositoryMode repositoryMode)
+        {
+            return false;
+        }
+
+        public IGherkinLinkProviderViewModel GetGherkinLinkViewModel(RepositoryMode repositoryMode)
+        {
+            return null;
+        }
+
+        public bool IsGherkinStatsProvider(RepositoryMode repositoryMode)
+        {
+            return false;
+        }
+
+        public IGherkinStatsProvider CreateStatsProvider(RepositoryMode repositoryMode, string configuration, IGherkinStatsRepository statsRepository)
+        {
+            return null;
+        }
+
+        public bool IsWorkItemSynchronizer(RepositoryMode repositorMode)
+        {
+            return false;
+        }
+
+        public IEnumerable<PluginCommand> GetSupportedCommands(RepositoryMode repositoryMode)
+        {
+            return Enumerable.Empty<PluginCommand>();
+        }
     }
 }
---
+++ SpecLog.HtmlExportPlugin.Client/HtmlExportPluginConfigDialogViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecLog.Application.Common.PluginsInfrastructure;
+
+namespace SpecLog.HtmlExportPlugin.Client
+{
+    public class HtmlExportPluginConfigDialogViewModel : PluginConfigurationDialogViewModel<HtmlExportPluginConfiguration>
+    {
+        public HtmlExportPluginConfigDialogViewModel(string config, bool enabled) : base(config, enabled) { }
+
+        public override string Caption
+        {
+            get { return "HTML Export Plugin Configuration"; }
+        }
+    }
+}
