be3baa565ec0dc64fa6b4094ccee8c2e7a449c83
zsoltzsigmondi <zszs@techtalk.at>
2013-08-13 09:48:41 +0200

    Initial steps in implementing the Server side

--- SpecLog.HtmlExportPlugin.Server/HtmlExportPlugin.cs
+++ SpecLog.HtmlExportPlugin.Server/HtmlExportPlugin.cs
@@ -2,10 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TechTalk.SpecLog.Commands;
+using TechTalk.SpecLog.Common;
+using TechTalk.SpecLog.Entities;
+using TechTalk.SpecLog.Server.Services.PluginInfrastructure;
 
 namespace SpecLog.HtmlExportPlugin.Server
 {
-    public class HtmlExportPlugin
+    [Plugin(HtmlExportPlugin.PluginName)]
+    public class HtmlExportPlugin : ServerPlugin
     {
+        public const string PluginName = "SpecLog.HtmlExportPlugin";
+
+        public override void OnStart()
+        {
+            Log(System.Diagnostics.TraceEventType.Information, "{0} started", PluginName);
+        }
+
+        public override void OnStop()
+        {
+            Log(System.Diagnostics.TraceEventType.Information, "{0} stopped", PluginName);
+        }
+
+        public override void PerformCommand(string commandVerb, string issuingUser)
+        {
+            // nop
+        }
+
+        public override IEnumerable<IPeriodicActivity> ActiveSynchronizers
+        {
+            get { return Enumerable.Empty<IPeriodicActivity>(); }
+        }
+
+        public override void AfterApplyCommand(RepositoryInfo repository, Command command)
+        {
+            // nop
+        }
+
+        public override void AfterUndoCommand(RepositoryInfo repository, Command command)
+        {
+            // nop
+        }
+
+        public override void BeforeApplyCommand(RepositoryInfo repository, Command command)
+        {
+            // nop
+        }
+
+        public override void BeforeUndoCommand(RepositoryInfo repository, Command command)
+        {
+            // nop
+        }
     }
 }
